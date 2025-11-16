463fc5ab666c8ecaa705adba5ca144626e740d35
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2025-11-16 12:39:54 +0100

    fix: add mean image example

--- examples/train_images_cond.jl
+++ examples/train_images_cond.jl
@@ -30,16 +30,16 @@ function randomly_set_unconditioned(
 end
 
 ### settings
-num_timesteps = 100
+num_timesteps = 1000
 seed = 2714
 dataset = :MNIST
-data_directory = "path\\to\\data"
+data_directory = "~/mnist"
 output_directory = joinpath("outputs", "$(dataset)_" * Dates.format(now(), "yyyymmdd_HHMM"))
 model_channels = 16
 learning_rate = 0.001
 batch_size = 32
 combine_embeddings = vcat
-num_epochs = 10
+num_epochs = 50
 prob_uncond = 0.2
 loss_type = Flux.mse;
 to_device = gpu # cpu or gpu
@@ -175,26 +175,34 @@ display(canvas_train)
 
 all_classes = collect(1:num_classes)
 # One-hot labels for each class for sampling
-labels_all = Float32.(Matrix(Flux.onehotbatch(all_classes, 1:num_classes)))
-X0_all = p_sample_loop(diffusion, labels_all; guidance_scale=2.0f0, to_device=to_device);
+pre_one_hot = zeros(10, 12)
+for i in 1:num_classes
+    pre_one_hot[1, i] = 0.0;
+    pre_one_hot[4, i] = 0.4;
+    pre_one_hot[7, i] = 0.6;
+end
+pre_one_hot[4, 11] = 1.0
+pre_one_hot[7, 12] = 1.0
+labels_all = pre_one_hot |> to_device
+X0_all = p_sample_loop(diffusion, labels_all; guidance_scale=1.0f0, to_device=to_device);
 X0_all = X0_all |> cpu;
 imgs_all = convert2image(trainset, X0_all[:, :, 1, :]);
-canvas_samples = plot([plot(imgs_all[:, :, i], title="digit=$(i-1)") for i in 1:num_classes]..., ticks=nothing)
+canvas_samples = plot([plot(imgs_all[:, :, i], title="digit=$(i-1)") for i in 1:12]..., ticks=nothing)
 savefig(canvas_samples, joinpath(output_directory, "samples.png"))
 display(canvas_samples)
 
-for label in 1:num_classes
-    println("press enter for next label")
-    readline()
-    # Build a labels matrix selecting the current class
-    labels_k = zeros(Float32, num_classes, 12)
-    labels_k[label, :] .= 1
-    X0 = p_sample_loop(diffusion, labels_k; guidance_scale=2.0f0, to_device=to_device)
-    X0 = X0 |> cpu
-    imgs = convert2image(trainset, X0[:, :, 1, :])
-    canvas = plot([plot(imgs[:, :, i]) for i in 1:12]..., plot_title="digit=$(label-1)", ticks=nothing)
-    display(canvas)
-end
+# for label in 1:num_classes
+#     println("press enter for next label")
+#     readline()
+#     # Build a labels matrix selecting the current class
+#     labels_k = zeros(Float32, num_classes, 12)
+#     labels_k[label, :] .= 1
+#     X0 = p_sample_loop(diffusion, labels_k; guidance_scale=2.0f0, to_device=to_device)
+#     X0 = X0 |> cpu
+#     imgs = convert2image(trainset, X0[:, :, 1, :])
+#     canvas = plot([plot(imgs[:, :, i]) for i in 1:12]..., plot_title="digit=$(label-1)", ticks=nothing)
+#     display(canvas)
+# end
 
 println("press enter to finish")
 readline()
